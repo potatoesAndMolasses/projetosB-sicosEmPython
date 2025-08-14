58e3aa3b7cd526b8d0ea500391e8dd2f4ba6de4d
Jan <129630363+potatoesAndMolasses@users.noreply.github.com>
2025-08-14 12:32:51 -0300

    Delete lista01_ex15.py

--- lista01_ex15.py
+++
@@ -1,8 +0,0 @@
-def nome(nan):
-    print("Boa tarde, {}! Como você está?".format(nan))
-
-def main():
-    nan = input("Digite seu primeiro nome: ")
-    nome(nan)
-if __name__=="__main__":
-    main()

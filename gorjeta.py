40e100d554730e2f5896d17f6ad4a84b405fa4cc
Jan <129630363+potatoesAndMolasses@users.noreply.github.com>
2025-08-14 12:33:30 -0300

    Delete gorjeta.py

--- gorjeta.py
+++
@@ -1,8 +0,0 @@
-def bill_split():
-    friends = int(input("Digite a quantidade de amigos: "))
-    amount = float(input("Digite o valor da conta: "))
-    daCasa = amount * 1.2
-    quantiaIndividual = daCasa / friends
-    print("O total com os 20% foi {:.2f} e cada um deve pagar {:.2f}".format(daCasa, quantiaIndividual))
-
-bill_split()

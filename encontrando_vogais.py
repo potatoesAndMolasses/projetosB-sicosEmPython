b26ccb9c9472f70bd78935edf301b6344f7d361d
Jan <129630363+potatoesAndMolasses@users.noreply.github.com>
2025-08-14 12:33:16 -0300

    Delete encontrando_vogais.py

--- encontrando_vogais.py
+++
@@ -1,8 +0,0 @@
-def contador():
-    contador = 0
-    frase = input("Digite uma frase: ")
-    for letra in frase.lower():
-        if letra in "aeiou":
-            contador += 1
-    print("A quantidade de vogais é {}".format(contador))
-contador()
